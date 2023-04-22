d8a0f6e424302853a6746c615ee121b03c864491
Aakankshap16 <aakankshap971@gmail.com>
2023-04-22 17:30:43 +0530

    ability to retrive data from table

--- SQL_EmployeePayrollProblem.sql
+++ SQL_EmployeePayrollProblem.sql
@@ -1,5 +1,5 @@
              /*!!!!! Welcome to EmployeePayrollProblem !!!!*/
-create table employee_payroll
+create table employee_payroll  /* create table */
 (
  Id int primary key identity,
  Name varchar(40) NOT NULL,
@@ -9,7 +9,11 @@ create table employee_payroll
 
 select * from employee_payroll
 
+/* insert values */
 insert into employee_payroll values('Ram', 50000, '2023-04-21'),
                                    ('Shyam', 20000, '2023-04-21'),
 								   ('Bhole', 70000, '2023-04-21'),
 								   ('Durga', 50000, '2023-04-21')
+
+select * from employee_payroll  /* retrive data */
+
